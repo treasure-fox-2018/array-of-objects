2c93ea716b582dc504e1c72645def66684dc6c58
fajartc02 <fajartc02@gmail.com>
2018-05-30 16:52:15 +0700

    modified array-of-objects

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ function createObj (name, phase, gender) {
   return resultArr
 }
 
+function getData (str) {
+  for (var a = 0; a < resultArr.length; a++) {
+    if (str === resultArr[a].name) {
+      return resultArr[a]
+    }
+  }
+}
+
 createObj('Akbar', 1, 'male')
-createObj('icha', 1, 'female')
+createObj('Icha', 1, 'female')
 createObj('adhit', 2, 'male')
 createObj('Tama', 2, 'male')
 createObj('Rifky', 3, 'male')
 console.log(resultArr)
+
+console.log(getData('Icha'))
